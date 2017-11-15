bfc19bbc3db20a14a62b8b60400c69a49d0d4f7a
monpeco <monpeco@gmail.com>
2017-11-15 02:23:17 +0000

    Add create contacts table

---
+++ module3/sql-script/Contacts.sql
@@ -0,0 +1,8 @@
+CREATE TABLE Contacts(
+    id INTEGER PRIMARY KEY,
+    FirstName VARCHAR(255),
+    LastName VARCHAR(255),
+    EmailAddress VARCHAR(255),
+    PhoneNumber VARCHAR(255),
+    ZipCode VARCHAR(255)
+);
